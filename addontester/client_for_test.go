4529d2f147d9a9f803645ddb5c69704200df3654
Viktor Benei <viktorbenei@bitrise.io>
2020-11-22 19:30:43 +0000

    unit test fix

--- addontester/client_for_test.go
+++ addontester/client_for_test.go
@@ -15,17 +15,21 @@ type testAddonClient struct {
 	retryResponseStatusCode         int
 	responseBody                    string
 	err                             error
+	loginRequestInfos               addonprovisioner.LoginRequestInfos
 }
 
 func (c *testAddonClient) AddonURL() string {
 	return c.addonURL
 }
+
 func (c *testAddonClient) AuthToken() string {
 	return c.authToken
 }
+
 func (c *testAddonClient) SSOSecret() string {
 	return c.ssoSecret
 }
+
 func (c *testAddonClient) Provision(params addonprovisioner.ProvisionRequestParams) (int, string, error) {
 	c.numberOfProvisionRequestsCalled = c.numberOfProvisionRequestsCalled + 1
 	if c.numberOfProvisionRequestsCalled == 1 {
@@ -33,12 +37,23 @@ func (c *testAddonClient) Provision(params addonprovisioner.ProvisionRequestPara
 	}
 	return c.retryResponseStatusCode, c.responseBody, c.err
 }
+
 func (c *testAddonClient) Deprovision(appSlug string) (int, string, error) {
 	return c.responseStatusCode, c.responseBody, c.err
 }
+
 func (c *testAddonClient) ChangePlan(params addonprovisioner.ChangePlanRequestParams, appSlug string) (int, string, error) {
 	return c.responseStatusCode, c.responseBody, c.err
 }
+
 func (c *testAddonClient) Login(params addonprovisioner.LoginRequestParams) (int, string, error) {
 	return c.responseStatusCode, c.responseBody, c.err
+}
+
+func (c *testAddonClient) Login(params addonprovisioner.LoginRequestParams) (int, string, error) {
+	return c.responseStatusCode, c.responseBody, c.err
+}
+
+func (c *testAddonClient) LoginRequestInfos(params addonprovisioner.LoginRequestParams) (addonprovisioner.LoginRequestInfos, error) {
+	return c.loginRequestInfos, c.err
 }
